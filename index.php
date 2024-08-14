2b67894d73eefe93b1f294802c3d673883e865ac
Emmanuel Alejandro Sandoval Vasquez <Onczuwu@gmail.com>
2024-08-13 19:59:04 -0600

    Creacion de index.php con estructura html

---
+++ index.php
@@ -0,0 +1,21 @@
+<!Doctype html>
+<html>
+    <head>
+        <title>Emmanuel Alejandro Sandoval Vasquez</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css" /> 
+        <link rel="stylesheet" href="css/style.css" />
+        <script src="js/jquery-3.7.1.min"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+
+    </head>
+    <body>
+
+
+    </body>
+    <fooder>
+
+
+    </fooder>
+
+
+</html>
